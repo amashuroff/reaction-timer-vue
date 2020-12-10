f012847bbad0a1dd0a9ef1607538c05d8f3f1aaf
Aleksei <amashuroff@gmail.com>
2020-12-10 13:13:20 +0300

    completed project

---
+++ src/components/Block.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="block" @click="stopTimer" v-if="showBlock">Click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  name: "Block",
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: null,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("reacted", this.reactionTime);
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background-color: #0faf87;
+  color: white;
+  text-transform: uppercase;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+  <p>{{ result }}: {{ score }}ms. {{ quote }}</p>
+</template>
+
+<script>
+export default {
+  name: "Result",
+  props: ["score", "isPlaying"],
+  data() {
+    return {
+      quote: null,
+      result: "Result",
+    };
+  },
+  mounted() {
+    this.showQuote();
+  },
+  updated() {
+    if (this.isPlaying) {
+      this.result = "Previos result";
+    } else {
+      this.result = "Result";
+    }
+    this.showQuote();
+  },
+  methods: {
+    showQuote() {
+      if (this.score >= 350) {
+        this.quote = "Slow!!!";
+      } else {
+        this.quote = "Incredible skills!";
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
